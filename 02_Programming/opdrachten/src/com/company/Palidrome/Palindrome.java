0b7de715a58b12a96f0d91004fadf2c75661bdec
FII3x <100418521+FII3x@users.noreply.github.com>
2022-07-05 09:08:58 +0200

    palindrome extra opdracht

--- 02_Programming/opdrachten/src/com/company/Palidrome/Palindrome.java
+++ 02_Programming/opdrachten/src/com/company/Palidrome/Palindrome.java
@@ -3,35 +3,36 @@ package com.company.Palidrome;
 import java.util.Arrays;
 
 public class Palindrome {
-    public static boolean Is(String word) {
+    public static boolean isPalindrome(String word) {
         for (var x = 0; x <= word.length() / 2; x++)
             if (word.charAt(x) != word.charAt(word.length() - 1 - x))
                 return false;
         return true;
     }
 
-    public static int Is(int width) {
+    public static int isPalindrome(int width) {
         // 3 -> 999 => 10 ^ 3 - 1
-        var maxWidth = (int) Math.pow(10, width) - 1;
-        // onder waardes 999 * 999
-        var max = (maxWidth * maxWidth);
-        // find het grootste nummer
+        var result = -1;
+        var max = (int) Math.pow(10, width) - 1;
         for (var x = max; x > 0; x--) {
-            if (Is(x + ""))
-                return x;
+            for (var y = max; y > 0; y--) {
+                var num = x * y;
+                if (num > result && isPalindrome(""+num))  {
+                    result = num;
+                }
+            }
         }
-        return 0;
+        return result;
     }
 
     public static void run() {
         //906609
         Arrays.stream(new int[]{3, 4})
-                .mapToObj(x -> x + " = " + Is(x))
+                .mapToObj(x -> x + " = " + isPalindrome(x))
                 .forEach(System.out::println);
         Arrays.stream(new String[]{"racecar", "noon", "Alex", "vork"})
-                .map(x -> x + " = " + Is(x))
+                .map(x -> x + " = " + isPalindrome(x))
                 .forEach(System.out::println);
-
     }
 
     /*vind de grootste palindroom Lager dan een integer * integer
